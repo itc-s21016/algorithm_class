ef0be2aada34f7587c2395b64bcdd17575ead8d5
itc-s21016 <s21016@std.it-college.ac.jp>
2023-05-17 10:21:49 +0900

    表の折返しをi%9 == 8にして整えた

--- Ex_Lesson2-1/prime_2.py
+++ Ex_Lesson2-1/prime_2.py
@@ -11,7 +11,7 @@ def hyou():
             count += 1
         else:
             s += "/,"
-        if i%10 == 9:
+        if i%9 == 8:
             s += "\n"
     print(s)
     print("素数は" + str(count) + "個です")
