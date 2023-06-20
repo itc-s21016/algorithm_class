c4de26840b727c8768e14330d8ea93067e96f0f5
itc-s21016 <s21016@std.it-college.ac.jp>
2023-06-20 11:26:46 +0900

    Lesson5-7終了

---
+++ Lesson7/Lesson7-1/euclidean_algorithm_2.py
@@ -0,0 +1,16 @@
+def euclid(a, b):
+    if b == 0:
+        return a
+    else:
+        return euclid(b, a % b)
+
+
+print("a≧bとなる自然数を入力してください")
+a = int(input("a="))
+b = int(input("b="))
+if b == 0:  # 0のときエラーを出す
+    print("0はだめです")
+    print("もう一度入力してください")
+    b = int(input("b="))
+
+print("それらの数の最大公約数は", euclid(a, b))
